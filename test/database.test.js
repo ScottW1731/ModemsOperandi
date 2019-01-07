54e437dd56b196a6f2fc010c18328cf61cca3f0d
Your Name <you@example.com>
2019-01-07 14:33:35 -0600

    from develop branch

---
+++ test/database.test.js
@@ -0,0 +1,8 @@
+var expect = require("chai").expect;
+//todo: include
+
+describe("canary test", function () {
+    it("should pass this canary test", function () {
+        expect(true).to.be.true;
+    });
+});

1582a9a5ce97fb0ff398df71162df97be43bdfad
Your Name <you@example.com>
2019-01-06 20:58:13 -0600

    test code to get started

---
+++ src/public/js/index.js
@@ -0,0 +1,23 @@
+// this is going to be a manual feature for inputing parts
+
+// CPU
+// require input from user submit box
+// append to page
+// repeat for below stuff
+// Cpu Cooler
+// Motherboard
+// Memory
+// Storage
+// Video Card
+// Case
+// Power Supply
+// Optical drive(s)
+// OS
+// Software
+// Case Fan(s)
+// Monitor
+// External Storage
+// Epansion Cards/ Networking
+// Peripherals
+// Accessories/ Other
+// Custom parts

76ec544a753fde9ec24746e6343c745cfa875bd1
Michael Preston <michael.n.preston@gmail.com>
2019-01-15 22:38:46 -0600

    moved sql files to db folder

---
+++ db/reports.sql
@@ -0,0 +1,6 @@
+## Full build report
+select c.name as customerName, b.id as buildId, b.name, sum(cost) as total, customerId 
+	from parts p 
+	join builds b
+	join customers c
+        on p.buildId = b.id && c.id = b.customerId;
---
+++ db/seed.sql
@@ -0,0 +1,6 @@
+## Seed Data                
+insert into customers (id, name) values (1138, "mike");
+insert into builds (id, customerId, name) values (12345, 1138, "super-special-awesum build");
+insert into parts (buildId, name, cost) values (12345, "Intel Core i3-8100", 118.99);
+insert into parts (buildId, name, cost) values (12345, "Gigabyte B360M DSH3H", 74.99);
+insert into parts (buildId, name, cost) values (12345, "MSI Radeon RX 580 GB Armor", 209.99);

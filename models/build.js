f99596644354714c7432faae4fc9d9ca7f48e9ab
Byron <byron.p.roberts@gmail.com>
2019-01-16 21:11:52 -0600

    models and passport

---
+++ models/build.js
@@ -0,0 +1,23 @@
+module.exports = function (sequelize, DataTypes) {
+    var Build = sequelize.define("Build", {
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [1]
+            }
+        }, 
+        category: {
+            type:DataTypes.STRING,
+            allowNull: true,
+        }
+        // TODO: Integrate customerId somehow!
+    });
+
+    Build.associate = function(models) {
+        Build.belongsTo(models.Customer, {
+            onDelete: "cascade"
+        });
+    };
+    return Build;
+};
---
+++ models/customer.js
@@ -0,0 +1,48 @@
+// Requiring bcrypt for password hashing. Using the bcrypt-nodejs version as the regular bcrypt module
+// sometimes causes errors on Windows machines
+var bcrypt = require("bcrypt-nodejs");
+// Creating our Customer model
+module.exports = function(sequelize, DataTypes) {
+  var Customer = sequelize.define("Customer", {
+    id:{type:DataTypes.INTEGER,
+        unique: true,
+        allowNull: false,
+    },
+    name:  { 
+        type:DataTypes.STRING,
+        allowNull: false,
+    },
+    // The email cannot be null, and must be a proper email before creation
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true
+      }
+    },
+    // The password cannot be null
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
+  });
+
+  // Creating a custom method for our Customer model. This will check if an unhashed password entered by the Customer can be compared to the hashed password stored in our database
+  Customer.prototype.validPassword = function(password) {
+    return bcrypt.compareSync(password, this.password);
+  };
+  // Hooks are automatic methods that run during various phases of the Customer Model lifecycle
+  // In this case, before a Customer is created, we will automatically hash their password
+  Customer.hook("beforeCreate", function(Customer) {
+    Customer.password = bcrypt.hashSync(Customer.password, bcrypt.genSaltSync(10), null);
+  });
+//  Associating Customer to many builds
+  Customer.associate = function(models) {
+      Customer.hasMany(models.Build, {
+          onDelete: "cascade"
+      });
+  };
+
+
+  return Customer;

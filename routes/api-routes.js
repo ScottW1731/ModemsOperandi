ba1a0262a5d2350efe2057ba71d2131a458050b5
Michael Preston <michael.n.preston@gmail.com>
2019-01-18 15:57:06 -0600

    Updated paths, removed old comments

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -2,29 +2,23 @@
 // api-routes.js - this file offers a set of routes for displaying and saving data to the db
 // *********************************************************************************
 
-// Dependencies
-// =============================================================
 var connection = require("../config/connection.js");
 
-// Routes
-// =============================================================
-module.exports = function(app) {
-  // Get all builds
-  app.get("/api/all", function(req, res) {
-    var dbQuery = "SELECT * FROM builds";
+module.exports = function (app) {
 
+        /*Builds*/
+  app.get("/api/builds/all", function(req, res) {
+    var dbQuery = `SELECT * FROM builds`;
     connection.query(dbQuery, function(err, result) {
       if (err) throw err;
       res.json(result);
     });
   });
 
-  // Add a builds
-  app.post("/api/new", function(req, res) {
-    console.log("builds Data:");
+    app.post("/api/builds/new", function(req, res) {
     console.log(req.body);
 
-    var dbQuery = "INSERT INTO builds (author, body, created_at) VALUES (?,?,?)";
+    var dbQuery = "INSERT INTO builds (customerId, name, buildType) VALUES (?,?,?)";
 
     connection.query(dbQuery, [req.body.author, req.body.body, req.body.created_at], function(err, result) {
       if (err) throw err;

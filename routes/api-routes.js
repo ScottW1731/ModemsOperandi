e0c4dbb38125ac4d628a7f843c6d5e4ff06654a9
Michael Preston <michael.n.preston@gmail.com>
2019-01-18 16:26:34 -0600

    eslinted

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -6,24 +6,24 @@ var connection = require("../config/connection.js");
 
 module.exports = function (app) {
 
-        /*Builds*/
-  app.get("/api/builds/all", function(req, res) {
-    var dbQuery = `SELECT * FROM builds`;
-    connection.query(dbQuery, function(err, result) {
-      if (err) throw err;
-      res.json(result);
+    /*Builds*/
+    app.get("/api/builds/all", function (req, res) {
+        var dbQuery = `SELECT * FROM builds`;
+        connection.query(dbQuery, function (err, result) {
+            if (err) throw err;
+            res.json(result);
+        });
     });
-  });
 
-    app.post("/api/builds/new", function(req, res) {
-    console.log(req.body);
+    app.post("/api/builds/new", function (req, res) {
+        console.log(req.body);
 
-    var dbQuery = "INSERT INTO builds (customerId, name, buildType) VALUES (?,?,?)";
+        var dbQuery = "INSERT INTO builds (customerId, name, buildType) VALUES (?,?,?)";
 
-    connection.query(dbQuery, [req.body.author, req.body.body, req.body.created_at], function(err, result) {
-      if (err) throw err;
-      console.log("builds Successfully Saved!");
-      res.end();
+        connection.query(dbQuery, [req.body.author, req.body.body, req.body.created_at], function (err, result) {
+            if (err) throw err;
+            console.log("builds Successfully Saved!");
+            res.end();
+        });
     });
-  });
 };

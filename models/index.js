272197fd410045ffd1e7957c090d0af1b8427bb0
Michael Preston <michael.n.preston@gmail.com>
2019-01-18 18:20:40 -0600

    'var env' is now 'var environment'; Updated to use config password if there is one, else use the .env's.

--- models/index.js
+++ models/index.js
@@ -2,30 +2,33 @@ var fs = require("fs");
 var path = require("path");
 var Sequelize = require("sequelize");
 var basename = path.basename(module.filename);
-var env = process.env.NODE_ENV || "development";
-var config = require(__dirname + "/../config/config.json")[env];
+var environment = process.env.NODE_ENV || "development";
+var config = require(__dirname + "/../config/config.json")[environment];
 var db = {};
 
-if (config.use_env_variable) {
-  var sequelize = new Sequelize(process.env[config.use_env_variable]);
+console.log(config)
+
+if (config.use_env_value) {
+    var sequelize = new Sequelize(process.env[config.use_env_value]);
 } else {
-  var sequelize = new Sequelize(config.database, config.username, config.password, config);
+    let password = config.password || process.env.Password;
+    var sequelize = new Sequelize(config.database, config.username, password, config);
 }
 
 fs
-  .readdirSync(__dirname)
-  .filter(function(file) {
-    return (file.indexOf(".") !== 0) && (file !== basename) && (file.slice(-3) === ".js");
-  })
-  .forEach(function(file) {
-    var model = sequelize["import"](path.join(__dirname, file));
-    db[model.name] = model;
-  });
+    .readdirSync(__dirname)
+    .filter(function (file) {
+        return (file.indexOf(".") !== 0) && (file !== basename) && (file.slice(-3) === ".js");
+    })
+    .forEach(function (file) {
+        var model = sequelize["import"](path.join(__dirname, file));
+        db[model.name] = model;
+    });
 
-Object.keys(db).forEach(function(modelName) {
-  if (db[modelName].associate) {
-    db[modelName].associate(db);
-  }
+Object.keys(db).forEach(function (modelName) {
+    if (db[modelName].associate) {
+        db[modelName].associate(db);
+    }
 });
 
 db.sequelize = sequelize;

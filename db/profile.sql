b4b8e39dc70e67f8ee382eb9a2869eefe6613372
Michael Preston <michael.n.preston@gmail.com>
2019-01-21 16:42:03 -0600

    Changed name of report to 'profile'

---
+++ db/profile.sql
@@ -0,0 +1,9 @@
+#Get all customer's build(s) details (profile)
+select c.name, b.name, b.category, p.name, p.cost
+	from customers c 
+    join builds b
+		on c.id = b.customerId
+    join build_parts_xref bps    
+		on b.id = bps.buildId
+    join parts p
+		on p.id = bps.partId;

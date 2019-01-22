fe45fde17e9b3dcfc58caf2daf332e16ea7d5a7b
Michael Preston <michael.n.preston@gmail.com>
2019-01-22 17:54:55 -0600

    Added email to select

--- db/profile.sql
+++ db/profile.sql
@@ -1,5 +1,5 @@
 #Get all customer's build(s) details (profile)
-select c.name, b.name, b.category, p.name, p.cost
+select c.name, c.email, b.name, b.category, p.name, p.cost
 	from customers c 
     join builds b
 		on c.id = b.customerId

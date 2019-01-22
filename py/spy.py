afeb340a5b16fbcbaa04d02b611007204ce0b976
Michael Preston <michael.n.preston@gmail.com>
2019-01-22 02:45:14 -0600

    Spacing and moved extract func

--- py/spy.py
+++ py/spy.py
@@ -10,8 +10,6 @@ import urllib.request
 print('running PC Part Picker spyder...')
 
 # find the stub that will give us the raw html for a build:
-
-
 def findStub(searchUrl):
     source_code = requests.get(searchUrl)
     plain_text = source_code.text
@@ -26,11 +24,29 @@ def findStub(searchUrl):
 
 def checkUrl(url): return urllib.request.urlopen(url).getcode() == 200
 
-
 def getSoup(url):
     source_code = requests.get(url)
     plain_text = source_code.text
     return BeautifulSoup(plain_text, features="html.parser")
+
+
+connection = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    passwd="birman",
+    auth_plugin='mysql_native_password',
+    port=3306,
+    database="pc_builder"
+)
+
+
+start = time.time()
+
+#### MAIN ####
+base = "https://pcpartpicker.com"
+prefabNum = "Qf4qqs"
+permalink = base + "/b/" + prefabNum
+print("permalink URL: " + permalink)
 
 
 def extract(permalink):
@@ -46,36 +62,6 @@ def extract(permalink):
     textarea = soup.findAll('textarea', {"id": "markup_text"})[0]
     print(textarea)
 
-
-# def click(img):
-#     if img:
-#         a_tag = img[0].parent
-#         href = a_tag.get('href')
-#         print (href)
-
-
-connection = mysql.connector.connect(
-    host="localhost",
-    user="root",
-    passwd="birman",
-    auth_plugin='mysql_native_password',
-    port=3306,
-    database="pc_builder"
-)
-
-# print(connection)
-# cursor = connection.cursor()
-# cursor.execute("SHOW DATABASES")
-# for db in cursor:
-#     print(db)
-
-start = time.time()
-
-#### MAIN ####
-base = "https://pcpartpicker.com"
-prefabNum = "Qf4qqs"
-permalink = base + "/b/" + prefabNum
-print("permalink URL: " + permalink)
 
 extract(permalink)
 
